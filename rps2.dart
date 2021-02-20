0f13eadd1f4c27939a24be9828b763abf73f5d17
MCStormGamer <jakob.kerze@gmail.com>
2021-02-20 19:40:36 +0100

    a

--- rps2.dart
+++ rps2.dart
@@ -68,44 +68,62 @@ void main() async {
     if (myChoice == RPS.rock && computersChoice == RPS.rock) {
       print('You chose rock and computer chose rock. It\'s a draw.');
       result = Result.rockIrock;
+      print('computers points : $computerPoints');
+      print('players points : $playerPoints');
     }
     if (myChoice == RPS.rock && computersChoice == RPS.paper) {
       print('You chose rock and computer chose paper. You lost.');
       result = Result.rockIpaper;
       computerPoints = computerPoints + 1;
+      print('computers points : $computerPoints');
+      print('players points : $playerPoints');
     }
     if (myChoice == RPS.rock && computersChoice == RPS.scissors) {
       print('You chose rock and computer chose scissors. You won.');
       result = Result.rockIscissors;
       playerPoints = playerPoints + 1;
+      print('computers points : $computerPoints');
+      print('players points : $playerPoints');
     }
     if (myChoice == RPS.paper && computersChoice == RPS.rock) {
       print('You chose paper and computer chose rock. You won.');
       result = Result.paperIrock;
       playerPoints = playerPoints + 1;
+      print('computers points : $computerPoints');
+      print('players points : $playerPoints');
     }
     if (myChoice == RPS.paper && computersChoice == RPS.paper) {
       print('You chose paper and computer chose paper. It\'s a draw.');
       result = Result.paperIpaper;
+      print('computers points : $computerPoints');
+      print('players points : $playerPoints');
     }
     if (myChoice == RPS.paper && computersChoice == RPS.scissors) {
       print('You chose paper and computer chose scissors. You lost.');
       result = Result.paperIscissors;
       computerPoints = computerPoints + 1;
+      print('computers points : $computerPoints');
+      print('players points : $playerPoints');
     }
     if (myChoice == RPS.scissors && computersChoice == RPS.rock) {
       print('You chose scissors and computer chose rock. You lost.');
       result = Result.scissorsIrock;
       computerPoints = computerPoints + 1;
+      print('computers points : $computerPoints');
+      print('players points : $playerPoints');
     }
     if (myChoice == RPS.scissors && computersChoice == RPS.paper) {
       print('You chose scissors and computer chose paper. You won.');
       result = Result.scissorsIpaper;
       playerPoints = playerPoints + 1;
+      print('computers points : $computerPoints');
+      print('players points : $playerPoints');
     }
     if (myChoice == RPS.scissors && computersChoice == RPS.scissors) {
       print('You chose scissors and computer chose scissors. It\'s a draw.');
       result = Result.scissorsIpaper;
+      print('computers points : $computerPoints');
+      print('players points : $playerPoints');
     }
     if (myChoice == RPS.nah) {
       result = Result.nah;
@@ -115,8 +133,7 @@ void main() async {
     }
 
     //print(result);
-    print('computers points : $computerPoints');
-    print('players points : $playerPoints');
+
   }
 }
 

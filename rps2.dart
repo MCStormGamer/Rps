67470c31a42aaad39691818cfaaa6022e7b88bcf
MCStormGamer <jakob.kerze@gmail.com>
2021-03-08 17:45:10 +0100

    Removed Result enum & its usages

--- rps2.dart
+++ rps2.dart
@@ -22,21 +22,10 @@ enum RPS {
   scissors,
   nah,
 }
-enum Result {
-  rockIrock,
-  rockIpaper,
-  rockIscissors,
-  paperIrock,
-  paperIpaper,
-  paperIscissors,
-  scissorsIrock,
-  scissorsIpaper,
-  scissorsIscissors,
-  nah
-}
+
 void main() async {
-  int computerPoints = 0;
-  int playerPoints = 0;
+  int computerPoints = 9;
+  int playerPoints = 9;
   final String INPUT_PROMPT = 'Rock, paper, scissors - which will it be? (r/p/s) ';
   final String WRONG_INPUT = 'Please use one of the given options!';
   while (computerPoints < 10 && playerPoints < 10) {
@@ -44,7 +33,7 @@ void main() async {
     RPS computersChoice;
     String input;
     final int randomInt = Random().nextInt(3);
-    Result result;
+
     while (myChoice == RPS.nah) {
       stdout.write(INPUT_PROMPT);
       input = stdin.readLineSync() ?? '';
@@ -70,72 +59,60 @@ void main() async {
 
     if (myChoice == RPS.rock && computersChoice == RPS.rock) {
       print('You chose rock and computer chose rock. It\'s a draw.');
-      result = Result.rockIrock;
       print('computers points : $computerPoints');
       print('players points : $playerPoints');
     }
     if (myChoice == RPS.rock && computersChoice == RPS.paper) {
       print('You chose rock and computer chose paper. You lost.');
-      result = Result.rockIpaper;
       computerPoints = computerPoints + 1;
       print('computers points : $computerPoints');
       print('players points : $playerPoints');
     }
     if (myChoice == RPS.rock && computersChoice == RPS.scissors) {
       print('You chose rock and computer chose scissors. You won.');
-      result = Result.rockIscissors;
       playerPoints = playerPoints + 1;
       print('computers points : $computerPoints');
       print('players points : $playerPoints');
     }
     if (myChoice == RPS.paper && computersChoice == RPS.rock) {
       print('You chose paper and computer chose rock. You won.');
-      result = Result.paperIrock;
       playerPoints = playerPoints + 1;
       print('computers points : $computerPoints');
       print('players points : $playerPoints');
     }
     if (myChoice == RPS.paper && computersChoice == RPS.paper) {
       print('You chose paper and computer chose paper. It\'s a draw.');
-      result = Result.paperIpaper;
       print('computers points : $computerPoints');
       print('players points : $playerPoints');
     }
     if (myChoice == RPS.paper && computersChoice == RPS.scissors) {
       print('You chose paper and computer chose scissors. You lost.');
-      result = Result.paperIscissors;
       computerPoints = computerPoints + 1;
       print('computers points : $computerPoints');
       print('players points : $playerPoints');
     }
     if (myChoice == RPS.scissors && computersChoice == RPS.rock) {
       print('You chose scissors and computer chose rock. You lost.');
-      result = Result.scissorsIrock;
       computerPoints = computerPoints + 1;
       print('computers points : $computerPoints');
       print('players points : $playerPoints');
     }
     if (myChoice == RPS.scissors && computersChoice == RPS.paper) {
       print('You chose scissors and computer chose paper. You won.');
-      result = Result.scissorsIpaper;
       playerPoints = playerPoints + 1;
       print('computers points : $computerPoints');
       print('players points : $playerPoints');
     }
     if (myChoice == RPS.scissors && computersChoice == RPS.scissors) {
       print('You chose scissors and computer chose scissors. It\'s a draw.');
-      result = Result.scissorsIpaper;
       print('computers points : $computerPoints');
       print('players points : $playerPoints');
     }
     if (myChoice == RPS.nah) {
-      result = Result.nah;
       print(WRONG_INPUT);
       stdout.write(INPUT_PROMPT);
       input = stdin.readLineSync() ?? '';
     }
-
-    //print(result);
   }
 }
 

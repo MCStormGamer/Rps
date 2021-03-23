ebeb518fa1b026e87c55d70568b9a8aa7f3bb4fa
MCStormGamer <jakob.kerze@gmail.com>
2021-03-23 18:08:45 +0100

    comments removed

--- rps2.dart
+++ rps2.dart
@@ -1,21 +1,6 @@
 import 'dart:io';
 import 'dart:math';
 
-// import 'package:cli_util/cli_logging.dart';
-// Logger logger = Logger.verbose();
-//
-// logger.stdout('Hello world!');
-// logger.trace('message 1');
-// await new Future.delayed(new Duration(milliseconds: 200));
-// logger.trace('message 2');
-// logger.trace('message 3');
-
-// Progress progress = logger.progress('doing some work');
-// await new Future.delayed(new Duration(seconds: 2));
-// progress.finish(showTiming: true);
-
-// logger.stdout('All ${logger.ansi.emphasized('done')}.');
-
 enum RPS {
   rock,
   paper,
